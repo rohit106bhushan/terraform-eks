f44f67f7c98aa48fe635a15d47a6a25626f8b8eb
Rohit Bhushan <58414896+rohit106bhushan@users.noreply.github.com>
2024-12-03 14:51:16 +0530

    Update vpc.tf
    changed cluster name

--- vpc.tf
+++ vpc.tf
@@ -7,7 +7,7 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
-  cluster_name = "abhi-eks-${random_string.suffix.result}"
+  cluster_name = "rohit-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
