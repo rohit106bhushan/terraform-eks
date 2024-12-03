37168bc6d7c61b3b8b4e969d1ebc2c189d1dc06b
Rohit Bhushan <58414896+rohit106bhushan@users.noreply.github.com>
2024-12-03 12:57:48 +0530

    Update vpc.tf
    changed following 
    
    module "vpc" {
      source  = "terraform-aws-modules/vpc/aws"
      version = "5.16.0"
    }

--- vpc.tf
+++ vpc.tf
@@ -17,7 +17,7 @@ resource "random_string" "suffix" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.7.0"
+  version = "5.16.0"
 
   name                 = "abhi-eks-vpc"
   cidr                 = var.vpc_cidr
