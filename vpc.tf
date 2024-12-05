df6780aca29b73e6f40d900a0b5bfbcd9947019d
Rohit Bhushan <58414896+rohit106bhushan@users.noreply.github.com>
2024-12-05 17:12:49 +0530

    Update vpc.tf
    rohit vpc

--- vpc.tf
+++ vpc.tf
@@ -19,7 +19,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.16.0"
 
-  name                 = "abhi-eks-vpc"
+  name                 = "rohit-eks-vpc"
   cidr                 = var.vpc_cidr
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
